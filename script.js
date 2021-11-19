67211811d3d5921ed243f1ae37582433a13af9da
saurabhchirde <saurabh123.chirde@gmail.com>
2021-11-19 17:29:43 +0530

    script: logic building

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 /* Create a password checker web app. If password is lesser than 10 characters then show an error to user otherwise show success. Someone can ask to make the submit button disabled. Some can ask to make the input field green or red depending on input. */
+const input = document.querySelector(".inputText");
+const btnCheck = document.querySelector(".check");
+const output = document.querySelector(".output");
+
+const check = () => {
+  const pass = input.value.split("");
+  if (pass.length < 10) {
+    input.style.backgroundColor = "red";
+    input.style.color = "white";
+    output.innerText = `Password in less than 10 characters, please add ${
+      10 - pass.length
+    } more character`;
+  } else {
+    input.style.backgroundColor = "green";
+    input.style.color = "white";
+    output.innerText = "Good Password 🤩";
+  }
+};
+
+btnCheck.addEventListener("click", check);
